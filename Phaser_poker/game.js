f3c40792786c33fd122c4639ddca5d4f00fcb23a
unknown <benjamin.choi@KyunghwaChoi-PC.hq.nsuslab.net>
2015-10-30 12:19:46 +0900

    poker game add

---
+++ Phaser_poker/game.js
@@ -0,0 +1,132 @@
+﻿Array.prototype.shuffle = function () {
+    var n = this.length;
+    while (n--) {
+        var i = Math.floor(n * Math.random());
+        var tmp = this[i];
+        this[i] = this[n];
+        this[n] = tmp;
+
+    }
+    return this;
+}
+
+GameStates.Game = function (game) {
+    console.log(game);
+    this.platforms = null;
+    this.cursors = null;
+    this.card = null;
+    this.nCurrentIndex = 0;
+    this.student = null;
+
+    this.suit = ['spades', 'hearts', 'diamonds', 'clubs'];
+    this.ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '1'];
+
+};
+
+GameStates.Game.prototype = {
+
+    create: function () {
+
+        this.game.add.sprite(0, 0, 'table');
+
+        this.platforms = this.game.add.group();
+        this.platforms.enableBody = true;
+        
+        this.init();
+        
+        this.avatarsEffect();
+       
+    },
+
+    init: function () {
+        
+        this.card = this.game.add.group();
+        var randomIndex = Math.floor(Math.random() * this.card.length);
+        for (var i = 0; i < this.suit.length; i++) {
+            for (var j = 0; j < this.ranks.length; j++) {
+
+                
+                var cards = this.card.create(this.world.centerX, -100 * j, this.ranks[j] + '_of_' + this.suit[i]);
+
+                cards.anchor.setTo(0.5, 0.5);
+                cards.scale.setTo(0.4, 0.4);
+                cards.inputEnabled = true;
+            }
+        }
+        this.card.children.shuffle();
+
+        for (var i = 0; i < this.card.children.length; i++) {
+            var card = this.card.children[i];
+            var cardAction = this.game.add.tween(card);
+            cardAction.to({ y: this.world.centerY }, 300, Phaser.Easing.Out, true);
+
+            card.events.onInputDown.add(this.clickedCards, this);
+        }
+
+        console.debug('this.card', this.card);
+    },
+
+    
+
+    clickedCards: function (selectCard) {
+
+        var selectCardAction = this.game.add.tween(selectCard),
+            xAniNum = 70,
+            yAniNum = this.world.centerY;
+
+
+        switch (this.nCurrentIndex % 4) {
+            case 0:
+                xAniNum = 70;
+                yAniNum = this.world.centerY;
+                this.student.animations.play('walkBack', 20, true);
+                break;
+
+            case 1:
+                xAniNum = this.world.centerX;
+                yAniNum = 80;
+                this.student.animations.play('walkSide1', 20, true);
+                break;
+            case 2:
+                xAniNum = 730;
+                yAniNum = this.world.centerY;
+                this.student.animations.play('walkSide2', 20, true);
+                break;
+            case 3:
+                xAniNum = this.world.centerX;
+                yAniNum = 520;
+                this.student.animations.play('walkFront', 20, true);
+                break;
+        }
+
+        selectCardAction.to({ x: xAniNum, y: yAniNum }, 300, Phaser.Easing.Out);
+        selectCardAction.start();
+
+        if (this.nCurrentIndex >= this.card.length) {
+            selectCardAction.stop();
+            this.student.animations.stop();
+            selectCard.inputEnabled = false;
+            console.log(selectCard)
+        } else {
+            this.nCurrentIndex++;
+        }
+
+        console.debug('nCurrentIndex', this.nCurrentIndex);
+;
+    },
+
+    avatarsEffect: function () {
+        this.student = this.game.add.tileSprite(0, 0, 64, 64, 'student');
+        this.student.animations.add('walkBack', [0, 1, 2, 3, 4, 5, 6, 7, 8]);
+        this.student.animations.add('walkSide1', [9, 10, 11, 12, 13, 14, 15, 16, 17]);
+        this.student.animations.add('walkFront', [18, 19, 20, 21, 22, 23, 24, 25, 26]);
+        this.student.animations.add('walkSide2', [27, 28, 29, 30, 31, 32, 33, 34, 35]);
+    },
+   
+    update: function () {
+    },
+
+    render: function () {
+     //   this.game.debug.inputInfo(32, 32);
+    },
+};
---
+++ Phaser_poker/preloader.js
@@ -0,0 +1,33 @@
+﻿// Preloader will load all of the assets like graphics and audio
+GameStates.Preloader = function (game) {
+    this.preloadBar = null;
+    this.suit = ['spades', 'clubs', 'diamonds', 'hearts'];
+    this.ranks = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'];
+}
+
+
+GameStates.Preloader.prototype = {
+    preload: function () {
+        // common to add a loading bar sprite here...
+        this.preloadBar = this.add.sprite(this.game.width / 2 - 100, this.game.height / 2, 'preloaderBar');
+        this.load.setPreloadSprite(this.preloadBar);
+        // load all game assets
+        // images, spritesheets, atlases, audio etc..
+        this.load.spritesheet('student', '/assets/spritesheets/student.png', 64, 64, 36);
+        this.load.image('table', '/assets/bg_platform.png');
+        
+        for (var i = 0; i < this.suit.length; i++) {
+            for (var j = 0; j < this.ranks.length; j++) {
+                this.load.image(this.ranks[j] + '_of_' + this.suit[i], '/assets/svg_card/' + this.ranks[j] + '_of_' + this.suit[i] + '.svg');
+            }
+            
+        }
+    },
+
+    create: function () {
+        //call next state
+        this.state.start('MainMenu');
+        
+        console.log('preloader');
+    }
+};
